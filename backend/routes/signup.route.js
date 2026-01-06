64258b62e4d6b611d1f4adc64c66a276f09eaabc
RajalakshmiSR10 <rithuchara@gmail.com>
2026-01-06 16:15:47 +0530

    Updated

--- backend/routes/signup.route.js
+++ backend/routes/signup.route.js
@@ -16,19 +16,3 @@ router.delete("/:id", deleteUser);
 export default router;
 
 
-// import express from "express";
-// import {
-//   signup,
-//   getUsersList,
-//   updateUser,
-//   deleteUser,
-// } from "../controllers/signup.controller.js";
-
-// const router = express.Router();
-
-// router.post("/", signup);
-// router.get("/getUsersList", getUsersList);
-// router.put("/:id", updateUser);
-// router.delete("/:id", deleteUser);
-
-// export default router;
